e11db56e837855b826caa7fa5f06d92b8240af3d
Vruchtel Serafima <vruchtel.serafima@yandex.ru>
2017-07-25 21:06:17 +0300

    Добавлено первое оконное приложение - Убегающее окно

---
+++ RandWindow/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RandWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вы стремитесь сделать мир лучше?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Да, конечно!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(467, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Нет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 175);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "First Window";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ RandWindow/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Переводим координаты указателя мыши в строки и записываем в поля ввода
+            textBox1.Text = e.X.ToString();
+            textBox2.Text = e.Y.ToString();
+
+            // Если координаты мыши лежат в очерчиваемом вокруг кнопки "да, конечно" прямоугольнике
+            if(e.X > 230 && e.X < 350 && e.Y > 100 && e.Y < 130)
+            {
+                // Запоминаем текущее положение окна
+                currentWindowLocation = this.Location;
+
+                // Генерируем смещение окна по осям
+                currentWindowLocation.X += rnd.Next(-200, 200);
+                currentWindowLocation.Y += rnd.Next(-200, 200);
+
+                // Если окно вылезло за пределы экрана по одной из осей
+                if (currentWindowLocation.X < 0 
+                    || currentWindowLocation.X > (_monitorWidth - this.Width) 
+                    || currentWindowLocation.Y < 0 
+                    || currentWindowLocation.Y > (_monitorHeight - this.Height))
+                {
+                    // Новыми координатами станет центр окна
+                    currentWindowLocation.X = _monitorWidth / 2;
+                    currentWindowLocation.Y = _monitorHeight / 2;
+                }
+
+                // Обновляем положение окна на новое сгенерированное
+                this.Location = currentWindowLocation;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы усердны!", "Поздравляем!");
+            // Завершить приложение
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Второй параметр - заголовок окна сообщения
+            // третий - тип размещаемой кнопки на форме сообщения
+            // четвёртый - тип сообщения (иконка + звуковой сигнал)
+            MessageBox.Show("Мы не сомневались в Вашем безразличии!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private Random rnd = new Random();
+        private Point currentWindowLocation;
+        private int _monitorWidth = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width;
+        private int _monitorHeight = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;
+
+    }
+}
